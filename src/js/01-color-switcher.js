4d8697b423b48dea56b5c1e91e75448b2d8ad734
JoannaAtam <joanna.atamanczuk@gmail.com>
2024-09-23 18:15:20 +0200

    calosc

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,27 @@
+
+function getRandomHexColor() {  
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+    }
+
+const startButton = document.querySelector('[data-start]');
+const stopButton = document.querySelector('[data-stop]');
+let colorChangeInterval = null;
+
+startButton.addEventListener('click', () => {
+
+    startButton.disabled = true;
+    stopButton.disabled = false;
+
+    colorChangeInterval = setInterval(() => {
+        document.body.style.background = getRandomHexColor();
+    }, 1000);
+
+})
+
+stopButton.addEventListener('click', () => {
+
+    startButton.disabled = false;
+    stopButton.disabled = true;
+
+    clearInterval(colorChangeInterval);
+})
