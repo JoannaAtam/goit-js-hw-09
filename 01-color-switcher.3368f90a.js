42b60a8dc58bda6ec85d359be199ea03ff5844fd
JoannaAtam <140481597+JoannaAtam@users.noreply.github.com>
2024-09-23 16:32:30 +0000

    Deploying to gh-pages from @ JoannaAtam/goit-js-hw-09@4d8697b423b48dea56b5c1e91e75448b2d8ad734 🚀

---
+++ 01-color-switcher.3368f90a.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]");let d=null;e.addEventListener("click",(()=>{e.disabled=!0,t.disabled=!1,d=setInterval((()=>{document.body.style.background=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3)})),t.addEventListener("click",(()=>{e.disabled=!1,t.disabled=!0,clearInterval(d)}));
+//# sourceMappingURL=01-color-switcher.3368f90a.js.map
